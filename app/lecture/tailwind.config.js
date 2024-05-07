d3ab80afd66332a9f78dd5093f82021c021249e2
reina <ryugureina314@gmail.com>
2024-05-08 00:03:13 +0900

    update: configure paths

--- app/lecture/tailwind.config.js
+++ app/lecture/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    content: ["./src/**/*.{vue,js,ts,jsx,tsx}"],
+    darkMode: true,
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 }
 
