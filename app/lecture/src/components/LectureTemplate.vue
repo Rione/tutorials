2bfc228a77e0fc1bc2e02f91f9649248c92b0234
reina <ryugureina314@gmail.com>
2024-05-08 00:05:12 +0900

    tmp

---
+++ app/lecture/src/components/LectureTemplate.vue
@@ -0,0 +1,17 @@
+<script setup>
+    import Lecture1 from './Lecture1.vue';
+    import Lecture2 from './Lecture2.vue';
+
+    defineProps({
+        number: String
+    })
+</script>
+
+<template>
+    <h1 class="text-white">{{ number }}</h1>
+    <!-- <Lecture1 v-if="$route.params.number === '1'"/> -->
+    <!-- <Lecture2 v-else-if="$route.params.number === '2'"/> -->
+</template>
+
+<style scoped>
+</style>
