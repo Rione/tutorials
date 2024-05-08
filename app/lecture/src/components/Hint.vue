dc388122fda750778ca5986c382bbf47692871ed
reina <ryugureina314@gmail.com>
2024-05-08 23:54:37 +0900

    update: change bg color

--- app/lecture/src/components/Hint.vue
+++ app/lecture/src/components/Hint.vue
@@ -12,9 +12,9 @@
             var targetValue = button.attr('target');
 
             if (!$(targetValue).is(':visible')) {
-                button.text('ヒントを隠す').removeClass('bg-green-600 hover:bg-green-500').addClass('bg-neutral-600 hover:bg-neutral-500');
+                button.text('ヒントを隠す').removeClass('bg-green-700 hover:bg-green-600').addClass('bg-neutral-600 hover:bg-neutral-500');
             } else {
-                button.text('ヒントを見る').removeClass('bg-neutral-600 hover:bg-neutral-500').addClass('bg-green-600 hover:bg-green-500');
+                button.text('ヒントを見る').removeClass('bg-neutral-600 hover:bg-neutral-500').addClass('bg-green-700 hover:bg-green-600');
             }
             $(targetValue).fadeToggle(800);
         });
@@ -23,7 +23,7 @@
 
 <template>
     <div class="mx-5">
-        <button :id="'hint-' + level + '-btn'" class="rounded p-1 bg-green-600 hover:bg-green-500" :target="'#hint-' + level">ヒントを見る</button>
+        <button :id="'hint-' + level + '-btn'" class="rounded p-1 bg-green-700 hover:bg-green-600" :target="'#hint-' + level">ヒントを見る</button>
         <div :id="'hint-' + level" class="hint">
             <p>Hint: <slot></slot></p>
         </div>
