d8ae45f4a14fac70da83d16d6c0ab00e18309101
reina <ryugureina314@gmail.com>
2024-05-08 10:24:12 +0900

    add: pyscript

---
+++ app/lecture/src/components/PyScript.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="container bg-black-800 border border-green-600 rounded mt-3 px-3 py-3">
+        <p class="text-md">Python3.11</p>
+        <div class="px-3 rounded bg-white text-black">
+            <py-repl>
+                <slot></slot>
+            </py-repl>
+        </div>
+    </div>
+</template>
