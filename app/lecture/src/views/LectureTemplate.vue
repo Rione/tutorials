2bd2163f576b30c333cbee745a0f96607dd53843
reina <ryugureina314@gmail.com>
2024-05-08 10:25:00 +0900

    mov

---
+++ app/lecture/src/views/LectureTemplate.vue
@@ -0,0 +1,12 @@
+<script setup>
+    import Lecture1 from './Lecture1.vue';
+    import Lecture2 from './Lecture2.vue';
+</script>
+
+<template>
+    <Lecture1 v-if="$route.params.number === '1'"/>
+    <Lecture2 v-else-if="$route.params.number === '2'"/>
+</template>
+
+<style scoped>
+</style>
