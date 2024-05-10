b5dc43c126a7a32575d90c215805cdd433d45797
reina <ryugureina314@gmail.com>
2024-05-10 09:26:49 +0900

    feat: 404 page

---
+++ app/lecture/src/views/NotExisting.vue
@@ -0,0 +1,8 @@
+<script setup>
+    import Title from '../components/Title.vue';
+</script>
+
+<template>
+    <Title>response.status === 404</Title>
+    <div class="mt-2">存在しないページです!</div>
+</template>
