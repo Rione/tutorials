2d1f7168341bf7452b2b27d0e2ba719d44576095
reina <ryugureina314@gmail.com>
2024-05-10 09:26:21 +0900

    add: router for each page

--- app/lecture/src/views/LectureTemplate.vue
+++ app/lecture/src/views/LectureTemplate.vue
@@ -1,12 +1,16 @@
 <script setup>
     import Lecture1 from './Lecture1.vue';
-    import Lecture2 from './Lecture2.vue';
+    // import Lecture2 from './Lecture2.vue';
+    import WorkInProgress from './WorkInProgress.vue'
 </script>
 
 <template>
     <Lecture1 v-if="$route.params.number === '1'"/>
-    <Lecture2 v-else-if="$route.params.number === '2'"/>
+    <!-- <Lecture2 v-else-if="$route.params.number === '2'"/> -->
+    <WorkInProgress v-else-if="$route.params.number === '2'" />
+    <WorkInProgress v-else-if="$route.params.number === '3'" />
+    <WorkInProgress v-else-if="$route.params.number === '4'" />
+    <WorkInProgress v-else-if="$route.params.number === '5'" />
+    <WorkInProgress v-else-if="$route.params.number === '6'" />
+    <WorkInProgress v-else />
 </template>
-
-<style scoped>
-</style>
