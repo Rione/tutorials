056259eaff5bca62f6cb604250fa9420f48fa697
reina <ryugureina314@gmail.com>
2024-05-12 10:06:42 +0900

    add: specify output dir

--- app/lecture/vite.config.js
+++ app/lecture/vite.config.js
@@ -3,6 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
     base: "./",
+    build: {
+        outDir: 'lecture'
+    },
     plugins: [
         vue({
             template: {
