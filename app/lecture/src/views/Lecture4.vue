57f7fcd6f9d6ad7103cacc95b9435971362e26c7
reina <ryugureina314@gmail.com>
2024-05-31 00:07:40 +0900

    feat: support for code block

--- app/lecture/src/views/Lecture4.vue
+++ app/lecture/src/views/Lecture4.vue
@@ -5,6 +5,7 @@
     import SubHeader from '../components/SubHeader.vue';
     import Paragraph from '../components/Paragraph.vue';
     import PyScript from '../components/PyScript.vue';
+    import Code from '../components/Code.vue';
     import Hint from '../components/Hint.vue';
     import ProgressFooter from '../components/ProgressFooter.vue';
 
@@ -31,7 +32,7 @@
 </script>
 
 <template>
-    <Title>第4講 Python: 関数、pipの利用</Title>
+    <Title>第4講 Python: 関数、ライブラリ</Title>
 
     <div class="container w-3/6 mx-auto mb-10 text-start">
         <Header>1.関数</Header>
@@ -74,7 +75,7 @@
 
                 <PyScript>
                     def say_hello():
-                        print("Hello!")
+                        print("Hoi zäme!")
 
                     say_hello()
                 </PyScript>
@@ -82,13 +83,27 @@
 
             <SubHeader>再帰関数</SubHeader>
             <Paragraph>
-                
+                再帰関数(<span class="text-yellow-400">recursive function</span>)とは、関数の処理内で自身を再帰的に呼び出すような関数のことである。計算機科学の文脈では、トップダウン型のアプローチの一つである分割統治法を実現するために使用される。早速、実例を見てみよう。
+
+                <PyScript>
+                    def sum(num):
+                        if num == 1:
+                            return 1
+                        else:
+                            return sum(num - 1) + num
+
+                    sum(100)
+                </PyScript>
+
+                上の例は、与えられた数までの自然数の総和を計算するプログラムである(第2講では、for文を使って実装した)。
+                見ての通り、sum()関数の中でsum()関数が再帰的に呼び出されている。
+                再帰関数を使うと、for文などを用いるときと比べてシンプルなコードになる傾向がある一方で、メモリ使用量が大きいことや、無限ループに陥らないように注意を払わなければならない欠点がある。
             </Paragraph>
 
             <SubHeader>ジェネレータ</SubHeader>
             <Paragraph>
                 関数の特殊な形態の一つとして、ジェネレータ(<span class="text-yellow-400">generator</span>)が挙げられる。
-                ジェネレータは、イテラブルオブジェクトを生成するという点で通常のPythonのコレクション(listやdictなど)に類似しているが、ステートをもつという点でクラスに類似しているとも言える。
+                ジェネレータは、イテラブルオブジェクトを生成するという点で通常のPythonのコレクション(listやdictなど)に類似しているが、ステートをもつ(=変数の値を保持できる)という点ではクラスに類似しているとも言える。
                 とりあえず、例を見てみよう。
 
                 <PyScript>
@@ -122,285 +137,142 @@
                 これは、特にメモリの使用量が大きくなりがちな再帰関数の代替手法として有効な性質である。
             </Paragraph>
 
+            <SubHeader>ラムダ式</SubHeader>
             <Paragraph>
+                最後に、ラムダ式(<span class="text-yellow-400">lambda function</span>)について触れておこう。ラムダ式は無名関数とも呼ばれ、わざわざ関数名を与えるほどではない処理の塊に対して、サブルーチンとしてのまとまりを与えるのに使われる。以下に例を示そう。
+
+                <PyScript>
+                    example = lambda x: x ** 2
+                    
+                    print(example(3))
+                </PyScript>
+
+                このように、<span class="text-yellow-400">lambda</span>句の後に引数を、コロンの後に戻り値を記述することで、簡易な関数を定義することができる。
+                ただし、上の例のようにラムダ式を変数に直接代入して使用することは一般に推奨されない。
+                デバッグの際に、def句を用いて関数を定義した場合にはその関数名がトレースバックに表示されるが、ラムダ式では単に&lt;lambda&gt;としか表示されず、トレーサビリティが低下するからである。
+            </Paragraph>
+
+            <Paragraph>
+                そこで、ラムダ式の実用的な使用例を次にいくつか挙げる。
+
+                <PyScript>
+                    # with lambda
+                    data = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+                    odd_number = list(filter(lambda x: x % 2 == 1, data))
+                    print(odd_number)
+                </PyScript>
+
+                <PyScript>
+                    # without lambda
+                    def is_odd(x):
+                        return x % 2 == 1
+
+                    data = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+                    odd_number = list(filter(is_odd, data))
+                    print(odd_number)
+                </PyScript>
+
+                上は、<span class="text-yellow-400">filter()</span>関数を用いた典型的な例である。
+                filter()関数では、内部的に第二引数のデータをイテレートし、第一引数の関数がTrueを返すような要素のみを戻り値に含める。
+
+                <PyScript>
+                    # with lambda
+                    data = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+                    result = list(map(lambda x: x * ((-1) ** x), data))
+                    print(result)
+                </PyScript>
+
+                <PyScript>
+                    # without lambda
+                    def square(x):
+                        return x * (-1) ** x
+
+                    data = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+                    result = list(map(square, data))
+                    print(result)
+                </PyScript>
+
+                上は、配列に<span class="text-yellow-400">map()</span>関数を適用した例である。
+                map()関数は、配列の各要素に対してある処理を実行したいときに用いることができる。
+                引数は、filter()関数と同様である。<br><br>
+                このように、ラムダ式は特定の処理・関数との親和性が高く、大抵の場合、上で見た例のように複数の処理に組み合わされて用いられるのが一般である。
+            </Paragraph>
+        </Paragraph>
+
+
+        <Header>2.ライブラリ</Header>
+        <Paragraph>
+            Pythonの特徴の一つとして、その豊富なライブラリ(<span class="text-yellow-400">library</span>)が挙げられる。
+            ライブラリとは、便利な関数やクラスがひとまとめになって提供されるパッケージの総称であり、Pythonに標準で搭載される標準ライブラリ(<span class="text-yellow-400">os</span>や<span class="text-yellow-400">math</span>、<span class="text-yellow-400">pip</span>など)と、インターネット等から入手できる外部ライブラリ(<span class="text-yellow-400">pandas</span>や<span class="text-yellow-400">Numpy</span>、<span class="text-yellow-400">OpenCV</span>など)に分けられる。
+            また、ライブラリと区別して、Pythonに元から用意されている関数(print, inputなど)やクラス(int, strなど)のことを組み込み関数、組み込み型と呼ぶ場合もある。
+
+
+            <SubHeader>ライブラリのインストール</SubHeader>
+            <Paragraph>
+                標準ライブラリはインストール不要だが、外部ライブラリはインターネットからのインストールが必要である。
+                ライブラリのインストールは、<span class="text-yellow-400">pip</span>などのパッケージインストーラを介して行うのが一般的である。
+
+                <Code><span class="text-green-600">pip</span> --version</Code>
+
+                上のコマンドで、使用している環境にpipがインストールされているか確認することができる。
+                もし"pip: not found"となる場合は、まずpip自体をインストールする必要がある。
+                各自、実行しているOSに適した方法でpipのインストールを行うこと。
+            </Paragraph>
+
+            <Paragraph>
+                pipがインストールされていることを確認したら、今度は外部ライブラリをインストールしてみよう。
+                以下のコマンドは、<span class="text-yellow-400">OpenCV</span>と呼ばれる、画像処理の際に用いる外部ライブラリをインストールする。
+
+                <Code><span class="text-green-600">pip</span> install opencv-python</Code>
+
+                その他、インストールしたいライブラリがある場合は、<a href="https://pypi.org/" class="underline text-blue-200">Python Package Index (PyPI)</a>で検索してみると良い。
+                インストールする際のコマンドをコピー&ペーストすることができる。
                 
             </Paragraph>
 
-            <SubHeader>配列とfor文</SubHeader>
+            <SubHeader>ライブラリのインポート</SubHeader>
             <Paragraph>
-                配列の全ての要素を順に取り出したい場合には、for文を用いる。
-                listはイテラブルオブジェクト(<span class="text-yellow-400">iterable object</span>)であるため、単にfor文のin句の直後に渡せば良い。  
+                ライブラリを使用するには、Pythonのコード内で明示的に呼び出しを宣言する必要がある。
+                例えば、以下のようにすること。
 
                 <PyScript>
-                    food = ['Apple', 'Banana', 'Cherry', 'Durian', 'Eggplant']
+                    import math
 
-                    for i in food:
-                        print(i)
+                    print(math.pi)
+                    print(math.sqrt(2))
                 </PyScript>
 
-                また、配列の全ての要素に対して変更を加えたい場合にもfor文を用いる。
-                ただし、上記の方法ではうまくいかない。
-                なぜなら、indexのiには配列の各要素のコピー(<span class="text-yellow-400">deep copy</span>)が渡されているため、参照元のデータを書き換えることができないからである。これについては、次項で詳説する。
-            </Paragraph>
-
-            <Paragraph>
-                従って、配列の全ての要素を書き換えるためには次のようにすること。
+                上のように、<span class="text-yellow-400">import</span>句に続けてライブラリ名を記述すれば良い。
+                原則として、import句はコードの先頭に置くこと。
+                また、ライブラリ名が自分の定義した関数やクラスの名前と衝突する場合や、名前が長すぎて都度記述するのが煩わしい場合には、ライブラリに別名を与えることができる。
+                以下がその例である。
 
                 <PyScript>
-                    food = ['Apple', 'Banana', 'Cherry', 'Durian', 'Eggplant']
+                    import datetime as dt
 
-                    for i in range(len(food)):
-                        food[i] = 'Meat'
-                    
-                    print(food)
+                    print(dt.datetime.now())
                 </PyScript>
 
-                <span class="text-yellow-400">len()</span>関数は、与えられたlistの要素の個数を返す。
-                つまり上の例では、foodの要素の個数は5個のため、len(food)はint型の5を返す。
-                これをrange()関数に渡すことで、listのindexに対応した数列が得られる。
-            </Paragraph>
-
-            <SubHeader>要素の追加、削除</SubHeader>
-            <Paragraph>
-                配列に要素を追加するためには、いくつかの方法がある。代表的なものを以下に挙げる。
+                上のように、<span class="text-yellow-400">as</span>句を付せば良い。
+                また、ライブラリから特定の関数だけをインポートしたい場合には、次のようにすることもできる。
 
                 <PyScript>
-                    food = ['Apple', 'Banana', 'Cherry', 'Durian', 'Eggplant']
+                    from platform import python_version
 
-                    food.append('Fennel')
-                    print(food)
+                    print(python_version())
                 </PyScript>
-
-                <PyScript>
-                    food = ['Apple', 'Banana', 'Cherry', 'Durian', 'Eggplant']
-
-                    food += ['Fennel']
-                    print(food)
-                </PyScript>
-
-                <PyScript>
-                    food = ['Apple', 'Banana', 'Cherry', 'Durian', 'Eggplant']
-
-                    food.insert(5, 'Fennel')
-                    print(food)
-                </PyScript>
-            </Paragraph>
-
-            <Paragraph>
-                配列の要素を削除するためにも、いくつかの方法がある。代表的なものを以下に挙げる。
-
-                <PyScript>
-                    food = ['Apple', 'Banana', 'Cherry', 'Durian', 'Eggplant']
-
-                    food.remove('Eggplant')
-                    print(food)
-                </PyScript>
-
-                <PyScript>
-                    food = ['Apple', 'Banana', 'Cherry', 'Durian', 'Eggplant']
-
-                    food.pop(4)
-                    print(food)
-                </PyScript>
-
-                <PyScript>
-                    food = ['Apple', 'Banana', 'Cherry', 'Durian', 'Eggplant']
-
-                    del food[4]
-                    print(food)
-                </PyScript>
-
-                <PyScript>
-                    food = ['Apple', 'Banana', 'Cherry', 'Durian', 'Eggplant']
-
-                    food = [i for i in food if i != 'Eggplant']
-                    print(food)
-                </PyScript>
-            </Paragraph>
-
-            <SubHeader>配列のコピー</SubHeader>
-            <Paragraph>
-                データのコピーには、<span class="text-yellow-400">deep copy</span>と<span class="text-yellow-400">shallow copy</span>の2種類が存在する。
-                一般の変数のコピーはdeep copyであるが、配列の場合にはshallow copyとなるため、注意を要する。
-                まずは、例をそれぞれ見てみよう。
-
-                <PyScript>
-                    a = 2
-                    b = a       # deep copy
-                    
-                    print(f'Before: a = {a}')
-                    print(f'Before: b = {b}')
-
-                    a = 3       # コピーの参照元を書き換え
-
-                    print(f'After: a = {a}')
-                    print(f'After: b = {b}')
-                </PyScript>
-
-                上のコードでは、一般の変数のコピー、すなわちdeep copyの例を示した。
-                deep copyは、コピー元のオブジェクト(変数)のみならず、メモリ上にある参照先の実体(インスタンス)ごとコピーを行うため、一方を書き換えても、他方のインスタンスが書き換わることはない。
-            </Paragraph>
-
-            <Paragraph>
-                では次に、shallow copyの例を見てみよう。
-
-                <PyScript>
-                    sequence1 = [0, 1, 2, 3, 5, 8, 13]
-                    sequence2 = sequence1           # shallow copy
-
-                    print(f'Before: seq1 = {sequence1}')
-                    print(f'Before: seq2 = {sequence2}')
-
-                    sequence1[0] = 1                # コピーの参照元を書き換え
-
-                    print(f'After: seq1 = {sequence1}')
-                    print(f'After: seq2 = {sequence2}')
-                </PyScript>
-
-                上の例は、コピーの参照元を書き換えると、コピーした配列の値も書き変わることを示している。
-                shallow copyでは、コピーされるのはコピー元のオブジェクト(変数)のみであり、実体となるメモリ上のデータは共有される。
-                つまり、同じデータに対して異なる変数名を使ってアクセスしていることに相違ない。
-            </Paragraph>
-
-            <Paragraph>
-                配列をdeep copyしたい場合には、copyモジュールの<span class="text-yellow-400">deepcopy()</span>関数を使うか、以下のように、for文で各要素を一つずつコピーする方法がある。
-
-                <PyScript>
-                    food = ['Apple', 'Banana', 'Cherry', 'Durian', 'Eggplant']
-                    new_food = [i for i in food]
-
-                    food[0] = 'Avocado'
-
-                    print(f'food: {food}')
-                    print(f'new_food: {new_food}')
-                </PyScript>
-            </Paragraph>
-
-            <SubHeader>おまけ: 多次元配列</SubHeader>
-            <Paragraph>
-                これまで見てきた配列は、すべて<span class="text-yellow-400">1次元配列</span>(one-dimensional array, linear array)であった。
-                つまり、要素が一方向のみに増えていく1次元的な配列である。
-                これに対し、要素がn方向に向かって増えていくものを一般に<span class="text-yellow-400">n次元配列</span>(n-dimensional array)と呼ぶ。
-                以下は、2次元配列の例である。
-
-                <PyScript>
-                    matrix = [
-                                [1, 2, 3],
-                                [4, 5, 6],
-                                [7, 8, 9]
-                            ]
-
-                    print(matrix[1][2])
-                </PyScript>
-
-                ここで、配列が数学のベクトルや行列と似た概念であることに気づいた人がいるかもしれない。
-                実際、計算科学の文脈では配列のことをベクトルと呼ぶ場合もあるようである。
+                
+                このように、<span class="text-yellow-400">from</span>句とimport句を組み合わせることによって、特定の関数だけをインポートし、当該ファイルの名前空間にある関数として持ち込むことができる。
+                したがって、ライブラリ名の<span class="text-yellow-400">platform</span>は、python_version()関数が呼び出される時点では省略されている。
             </Paragraph>
         </Paragraph>
 
 
-        <Header>2.辞書</Header>
-        <Paragraph>
-            配列について理解できれば、辞書(<span class="text-yellow-400">dictionary</span>)を理解するのはそれほど難しいことではない。なぜなら、辞書は配列のindexの代わりに任意のキー(<span class="text-yellow-400">key</span>)を用いるに過ぎないからである。
-
-            <SubHeader>辞書の基本</SubHeader>
-            <Paragraph>
-                とりあえず、辞書の実例を見てみるとしよう。
-
-                <PyScript>
-                    me = {"name": "John", "age": 10, "hobby": "video game"}
-
-                    print(me["name"])
-                </PyScript>
-
-                このようにして、辞書は各要素にkey("name", "age", "hobby")を割り当てることができる。
-                要素にアクセスする際は、indexの代わりにkeyを用いる。
-                要素の順番を意識する必要がある配列とは異なり、keyを使用することで、任意の要素を絶対的に取り出すことができる。
-                当然ながら、keyは一意でなければならない。
-            </Paragraph>
-
-            <Paragraph>
-                keyを使うということ以外は、ほとんど配列と変わらない。
-                以下は、辞書の要素を書き換える例である。
-
-                <PyScript>
-                    me = {"name": "John", "age": 10, "hobby": "video game"}
-
-                    me["name"] = "Tom"
-                    print(me["name"])
-                </PyScript>
-            </Paragraph>
-
-            <SubHeader>辞書とfor文</SubHeader>
-            <Paragraph>
-                辞書には、要素の他にkeyが存在するため、for文ではその両方を取り出すことが可能である。
-                以下の例を見てみよう。
-
-                <PyScript>
-                    me = {"name": "John", "age": 10, "hobby": "video game"}
-
-                    for k, v in me.items():
-                        print(f'{k}: {v}')
-                </PyScript>
-
-                配列のように、辞書単体をfor文に渡すことはできず、辞書のクラスメソッドである<span class="text-yellow-400">items()</span>関数を先に呼び出さねばならない。
-                すると、for文の1番目のindex(k)にはkeyが、2番目のindex(v)には要素の値(value)が渡される。
-            </Paragraph>
-
-            <SubHeader>要素の追加、削除</SubHeader>
-            <Paragraph>
-                辞書に要素を追加するためには、keyを指定する必要があることに注意しよう。
-                以下に代表的な方法を示す。
-
-                <PyScript>
-                    desktop = {"os": "Windows 11", "cpu": "Core i9-14900K"}
-
-                    desktop["gpu"] = "RTX 4090"
-                    print(desktop)
-                </PyScript>
-
-                <PyScript>
-                    desktop = {"os": "Windows 11", "cpu": "Core i9-14900K"}
-
-                    desktop.update(gpu="RTX 4090")
-                    print(desktop)
-                </PyScript>
-
-                <PyScript>
-                    desktop = {"os": "Windows 11", "cpu": "Core i9-14900K"}
-
-                    desktop.update({"gpu": "RTX 4090"})
-                    print(desktop)
-                </PyScript>
-            </Paragraph>
-
-            <SubHeader>配列と辞書</SubHeader>
-            <Paragraph>
-                配列と辞書は、互いに入れ子になることが可能である。
-                具体例を見てみるとしよう。
-
-                <PyScript>
-                    arr1 = [1, 2, 3]
-                    arr2 = [4, 5, 6]
-                    dict = {"array1": arr1, "array2": arr2}
-
-                    print(dict)
-                </PyScript>
-
-                <PyScript>
-                    person1 = {"name": "Mike", "age": 50}
-                    person2 = {"name": "Josh", "age": 20}
-                    arr = [person1, person2]
-                    
-                    print(arr)
-                </PyScript>
-
-                このようにして、多重なる階層を持つ配列・辞書を作ることも可能である。
-            </Paragraph>
-        </Paragraph>
-
-
-        <Header>3.練習問題</header>
+        <Header>3.最終問題</header>
         <Paragraph>
             近日公開! 乞うご期待
         </Paragraph>
